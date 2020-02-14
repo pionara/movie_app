0aa7d1a52a96a0f0efd25e261b6499c0be089aee
pionara <miny3120@naver.com>
2020-02-14 10:16:00 +0900

    Updates

---
+++ precache-manifest.a1b1f56bc46ae55cc5954bd5ffe65f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d69da2653c4a7c463ffd2423e0e1e6b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "965f066efaf54c6162be",
+    "url": "/movie_app/static/css/main.669aa70f.chunk.css"
+  },
+  {
+    "revision": "fdb8badd6c6733b62810",
+    "url": "/movie_app/static/js/2.01bd4979.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.01bd4979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965f066efaf54c6162be",
+    "url": "/movie_app/static/js/main.98485e72.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
